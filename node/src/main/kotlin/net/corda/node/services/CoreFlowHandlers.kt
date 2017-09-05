f5750b54074da63124a12b5f4ed75f466bf519c9
Ross Nicoll <ross.nicoll@r3.com>
2017-09-05 15:41:15 +0100

    Rework TransactionKeyFlow to use SerializedBytes

--- node/src/main/kotlin/net/corda/node/services/CoreFlowHandlers.kt
+++ node/src/main/kotlin/net/corda/node/services/CoreFlowHandlers.kt
@@ -5,6 +5,9 @@ import net.corda.core.crypto.DigitalSignature
 import net.corda.core.crypto.secureRandomBytes
 import net.corda.core.flows.*
 import net.corda.core.identity.Party
+import net.corda.core.identity.PartyAndCertificate
+import net.corda.core.serialization.SerializedBytes
+import net.corda.core.serialization.serialize
 import net.corda.core.transactions.SignedTransaction
 import net.corda.core.utilities.ProgressTracker
 import net.corda.core.utilities.unwrap
@@ -64,11 +67,11 @@ class TransactionKeyHandler(val otherSide: Party, val revocationEnabled: Boolean
         val revocationEnabled = false
         progressTracker.currentStep = SENDING_KEY
         val legalIdentityAnonymous = serviceHub.keyManagementService.freshKeyAndCert(serviceHub.myInfo.legalIdentityAndCert, revocationEnabled)
-        val data = TransactionKeyFlow.buildDataToSign(legalIdentityAnonymous, theirNonce)
+        val serializedIdentity = SerializedBytes<PartyAndCertificate>(legalIdentityAnonymous.serialize().bytes)
+        val data = TransactionKeyFlow.buildDataToSign(serializedIdentity, theirNonce)
         val ourSig: DigitalSignature = serviceHub.keyManagementService.sign(data, legalIdentityAnonymous.owningKey)
-        sendAndReceive<TransactionKeyFlow.IdentityWithSignature>(otherSide, TransactionKeyFlow.IdentityWithSignature(legalIdentityAnonymous, ourSig.bytes)).unwrap { (confidentialIdentity, theirSigBytes) ->
-            val theirSig = DigitalSignature.WithKey(confidentialIdentity.owningKey, theirSigBytes)
-            TransactionKeyFlow.validateAndRegisterIdentity(serviceHub.identityService, otherSide, confidentialIdentity, ourNonce, theirSig)
+        sendAndReceive<TransactionKeyFlow.IdentityWithSignature>(otherSide, TransactionKeyFlow.IdentityWithSignature(serializedIdentity, ourSig.bytes)).unwrap { (confidentialIdentity, theirSigBytes) ->
+            TransactionKeyFlow.validateAndRegisterIdentity(serviceHub.identityService, otherSide, confidentialIdentity, ourNonce, theirSigBytes)
         }
     }
 }
